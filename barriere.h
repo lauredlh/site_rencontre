a0457265fb03119042a92d532d77c6c8bc2cdadf
lauredlh <delahaye.laure@outlook.fr>
2017-09-22 19:29:52 +0200

    Create barriere.h

---
+++ barriere.h
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+#define ECRIRE_INT(x) printf("%d\n",x);
+#define ECRIRE_FLOAT(x) printf("%f\n",x);
+#define ECRIRE_MESSAGE(x) printf("%s\n",x);
+#define LIRE_INT(x) scanf("%d",&x);
+#define LIRE_FLOAT(x) scanf("%f",&x);
